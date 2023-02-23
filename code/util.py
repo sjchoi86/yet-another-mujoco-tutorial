831de8116a5ce0fd7d16fa127f93497ec2bdd2a4
sjchoi86 <sj@SJ-MBP16-4TB.local>
2023-02-24 08:54:23 +0900

    Added IK and collision checker

---
+++ code/util.py
@@ -0,0 +1,145 @@
+import math
+import numpy as np
+from scipy.spatial.distance import cdist
+
+def pr2t(p,R):
+    """ 
+        Convert pose to transformation matrix 
+    """
+    p0 = p.ravel() # flatten
+    T = np.block([
+        [R, p0[:, np.newaxis]],
+        [np.zeros(3), 1]
+    ])
+    return T
+
+def t2pr(T):
+    """
+        T to p and R
+    """   
+    p = T[:3,3]
+    R = T[:3,:3]
+    return p,R
+
+def t2p(T):
+    """
+        T to p 
+    """   
+    p = T[:3,3]
+    return p
+
+def t2r(T):
+    """
+        T to R
+    """   
+    R = T[:3,:3]
+    return R    
+
+def rpy2r(rpy):
+    """
+        roll,pitch,yaw in radian to R
+    """
+    roll  = rpy[0]
+    pitch = rpy[1]
+    yaw   = rpy[2]
+    Cphi  = np.math.cos(roll)
+    Sphi  = np.math.sin(roll)
+    Cthe  = np.math.cos(pitch)
+    Sthe  = np.math.sin(pitch)
+    Cpsi  = np.math.cos(yaw)
+    Spsi  = np.math.sin(yaw)
+    R     = np.array([
+        [Cpsi * Cthe, -Spsi * Cphi + Cpsi * Sthe * Sphi, Spsi * Sphi + Cpsi * Sthe * Cphi],
+        [Spsi * Cthe, Cpsi * Cphi + Spsi * Sthe * Sphi, -Cpsi * Sphi + Spsi * Sthe * Cphi],
+        [-Sthe, Cthe * Sphi, Cthe * Cphi]
+    ])
+    assert R.shape == (3, 3)
+    return R
+
+def r2rpy(R,unit='rad'):
+    """
+        Rotation matrix to roll,pitch,yaw in radian
+    """
+    roll  = math.atan2(R[2, 1], R[2, 2])
+    pitch = math.atan2(-R[2, 0], (math.sqrt(R[2, 1] ** 2 + R[2, 2] ** 2)))
+    yaw   = math.atan2(R[1, 0], R[0, 0])
+    if unit == 'rad':
+        out = np.array([roll, pitch, yaw])
+    elif unit == 'deg':
+        out = np.array([roll, pitch, yaw])*180/np.pi
+    else:
+        out = None
+        raise Exception("[r2rpy] Unknown unit:[%s]"%(unit))
+    return out    
+
+def r2w(R):
+    """
+        R to \omega
+    """
+    el = np.array([
+            [R[2,1] - R[1,2]],
+            [R[0,2] - R[2,0]], 
+            [R[1,0] - R[0,1]]
+        ])
+    norm_el = np.linalg.norm(el)
+    if norm_el > 1e-10:
+        w = np.arctan2(norm_el, np.trace(R)-1) / norm_el * el
+    elif R[0,0] > 0 and R[1,1] > 0 and R[2,2] > 0:
+        w = np.array([[0, 0, 0]]).T
+    else:
+        w = np.math.pi/2 * np.array([[R[0,0]+1], [R[1,1]+1], [R[2,2]+1]])
+    return w.flatten()
+
+def trim_scale(x,th):
+    """
+        Trim scale
+    """
+    x         = np.copy(x)
+    x_abs_max = np.abs(x).max()
+    if x_abs_max > th:
+        x = x*th/x_abs_max
+    return x
+
+def soft_squash(x,x_min=-1,x_max=+1,margin=0.1):
+    """
+        Soft squashing numpy array
+    """
+    def th(z,m=0.0):
+        # thresholding function 
+        return (m)*(np.exp(2/m*z)-1)/(np.exp(2/m*z)+1)
+    x_in = np.copy(x)
+    idxs_upper = np.where(x_in>(x_max-margin))
+    x_in[idxs_upper] = th(x_in[idxs_upper]-(x_max-margin),m=margin) + (x_max-margin)
+    idxs_lower = np.where(x_in<(x_min+margin))
+    x_in[idxs_lower] = th(x_in[idxs_lower]-(x_min+margin),m=margin) + (x_min+margin)
+    return x_in    
+
+def soft_squash_multidim(
+    x      = np.random.randn(100,5),
+    x_min  = -np.ones(5),
+    x_max  = np.ones(5),
+    margin = 0.1):
+    """
+        Multi-dim version of 'soft_squash' function
+    """
+    x_squash = np.copy(x)
+    dim      = x.shape[1]
+    for d_idx in range(dim):
+        x_squash[:,d_idx] = soft_squash(
+            x=x[:,d_idx],x_min=x_min[d_idx],x_max=x_max[d_idx],margin=margin)
+    return x_squash 
+
+def kernel_se(X1,X2,hyp={'g':1,'l':1}):
+    """
+        Squared exponential (SE) kernel function
+    """
+    K = hyp['g']*np.exp(-cdist(X1,X2,'sqeuclidean')/(2*hyp['l']*hyp['l']))
+    return K
+
+def kernel_levse(X1,X2,L1,L2,hyp={'g':1,'l':1}):
+    """
+        Leveraged SE kernel function
+    """
+    K = hyp['g']*np.exp(-cdist(X1,X2,'sqeuclidean')/(2*hyp['l']*hyp['l']))
+    L = np.cos(np.pi/2.0*cdist(L1,L2,'cityblock'))
+    return np.multiply(K,L)
